4c69e59766f8c28de34751b7383fbd9b2aeb72fa
lyndemberg <lyndembergbatista@outlook.com>
2019-03-13 19:18:41 -0300

    estrutura do maven

---
+++ questao_01/share/src/main/java/sd/ifpb/App.java
@@ -0,0 +1,13 @@
+package sd.ifpb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
