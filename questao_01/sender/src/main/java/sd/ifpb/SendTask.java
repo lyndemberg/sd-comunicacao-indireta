0369783c2ef44e0204502d6eb1650d4bf4750487
lyndemberg <lyndembergbatista@outlook.com>
2019-03-13 21:30:05 -0300

    adicionando hosts no getRegistry das aplicaçoes

--- questao_01/sender/src/main/java/sd/ifpb/SendTask.java
+++ questao_01/sender/src/main/java/sd/ifpb/SendTask.java
@@ -28,7 +28,7 @@ public class SendTask extends TimerTask {
 			List<Message> list = repository.list();
 			//checar se existe mensagem
 			if (list.size() > 0){
-				Registry registry = LocateRegistry.getRegistry(10991);
+				Registry registry = LocateRegistry.getRegistry("host-server",10991);
 				//fazer o lookup
 				IReceiver receiver = (IReceiver) registry.lookup("Receiver");
 				//
